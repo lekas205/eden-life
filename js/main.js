3bc293ddf0c3c68bfa54413fde0f63b5c86cbc1e
lekas205 <lekass205@gmail.com>
2023-02-20 12:33:26 +0100

    added a function for the switch tab, also change navigation bar icon

--- js/main.js
+++ js/main.js
@@ -1,4 +1,16 @@
 "use strict"
+
+let switchTab = document.getElementsByClassName('switch_tab_botton')
+for(let i =0; i < switchTab.length; i++){
+    switchTab[i].addEventListener('click',(e)=>{
+        if(!e.target.classList.contains('active')){
+            let activeTab = document.querySelector('.switch_tab .active')
+            activeTab.classList.remove('active')
+            e.target.classList.add('active')
+        }
+    })
+}
+
 function openModalDialog(type) {
     let modal = document.querySelector('#app_modal')
     let modalContainer = document.querySelector('#app_modal .modal_body')
